20853cfcb8028a12ebf1b16bd23ddb5a37741ae1
Irving Guadalupe <2020030310@upsin.edu.mx>
2023-03-06 18:44:47 -0700

    Carpeta public

---
+++ public/js/limpiar.js
@@ -0,0 +1,8 @@
+document.getElementById('reset').addEventListener('click', ()=>{
+    window.location.href=window.location.href
+})
+
+if(nombre)
+document.getElementById('submit').addEventListener('click', ()=>{
+    window.alert("Se trató de enviar")
+})
